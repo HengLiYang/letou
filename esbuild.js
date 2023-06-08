4bdf36ae6583427ea3b32661058f1a22500dc2d9
HengLiYang <13834738983@163.com>
2023-06-08 15:00:15 +0800

    fix: 3

--- esbuild.js
+++ esbuild.js
@@ -1,20 +1,9 @@
 // const esbuild = require('esbuild');
 import * as esbuild from 'esbuild';
-// esbuild
-//   .build({
-//     entryPoints: ['app.jsx'],
-//     outdir: 'dist',
-//     bundle: true,
-//     sourcemap: true,
-//     minify: true,
-//     splitting: true,
-//     format: 'esm',
-//     target: ['esnext']
-//   })
-//   .catch(() => process.exit(1));
-let ctx = await esbuild.context({
-  entryPoints: ['app.jsx'],
-  bundle: true,
-  outdir: 'dist'
-});
-await ctx.watch();
+esbuild
+  .build({
+    entryPoints: ['app.jsx'],
+    bundle: true,
+    outfile: 'out.js'
+  })
+  .catch(() => process.exit(1));
